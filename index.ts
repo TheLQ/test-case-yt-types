41e5a7d4d17723f459e8d7301b31b13fed93ad0e
Leon Blakey <code@leonblakey.com>
2021-09-06 14:59:19 -0400

    added people, .d.ts example

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-const request = gapi.client.youtube.subscriptions.list({'part': 'snippet', 'mine': 'true'});
+gapi.client.youtube.subscriptions.list({'part': 'snippet', 'mine': 'true'});
+
+gapi.client.youtube2.subscriptions.asdf3({'part': 'snippet', 'mine': 'true'});
+
+gapi.client.people.people.get({
+  personFields: "Test string",
+  "requestMask.includeField": "Test string",
+  resourceName: "Test string",
+  sources: "Test string",
+});
